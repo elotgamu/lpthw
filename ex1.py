1a92d1edc7006e491400b6e1c180d25830088186
Eleazar Gaitan <elotgamu@gmail.com>
2018-02-10 20:11:03 -0600

    First exercised pushed to the repo

---
+++ ex1.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+print("Hello again")
+print("I like typingt this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rater you 'not'.")
+print('I "said" do not touch this.')
+
---
+++ ex5.py
@@ -0,0 +1,21 @@
+my_name = 'Eleazar Gaitan'
+my_age = 23  # not a lie
+my_height = 162  # centimeters
+my_weight = 165  # lbs
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+my_height_in_meters = my_height / 100
+my_weight_in_kg = my_weight / 2.2
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} meters tall.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# This one is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, {my_weight} I get {total}.")
+print(f"My height in meters is {my_height_in_meters}.")
+print(f"My wieight in kilograms is {my_weight_in_kg}.")
